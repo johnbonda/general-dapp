71b0143c54f1d49d7fe8bc2606be990ccfc289a9
johnbonda <john@belfricsbt.com>
2019-03-29 17:14:13 +0530

    What the fak

---
+++ init.js
@@ -0,0 +1,57 @@
+var mysqlite3 = require('sqlite3');
+var path = require('path');
+var util = require('./utils/util')
+
+module.exports = async function () {
+  console.log('enter dapp init')
+
+  app.registerContract(1000, 'domain.register')
+  app.registerContract(1001, 'domain.set_ip')
+  app.registerContract(1003, 'payroll.issuePaySlip')
+  app.registerContract(1004, 'payroll.verify')
+  app.registerContract(1006, 'temp.insertIntoEmployees')
+  app.registerContract(1007, 'payroll.registerEmployee')
+  app.registerContract(1008, 'payroll.authorize')
+  app.registerContract(1009, 'payroll.registerUser')
+  app.registerContract(1010, 'temp.saveTransactionId')
+  //app.registerContract(1005, 'payroll.pay')
+  //app.registerFee(1005, '0', 'BEL')
+  app.registerFee(1003, '0', 'BEL')
+  app.registerFee(1004, '0', 'BEL')
+  app.registerFee(1006, '0', 'BEL')
+  app.registerFee(1007, '0', 'BEL')
+  app.registerFee(1008, '0', 'BEL')
+  app.registerFee(1009, '0', 'BEL')
+  app.registerFee(1010, '0', 'BEL')
+  
+  app.events.on('newBlock', (block) => {
+    console.log('new block received', block.height)
+  })
+
+  app.sideChainDatabase = new mysqlite3.Database(path.join(__dirname, "blockchain.db"), (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Connected to the blockchain database');
+  });
+
+  var settingExists =  app.model.Setting.exists({
+    id: '0'
+  });
+  settingExists.then(function(data){
+    if(!data) 
+    app.sdb.create('setting', {
+      id: '0',
+      fields: JSON.stringify({
+        name: "Name",
+        id: "ID",
+        year: "Year",
+        degree: "Degree",
+        department: "Department"
+      }),
+      identity: JSON.stringify({
+        "Aadhar Card": "AdharNumber"
+      })
+    })
+  })
+}
---
+++ interface/contractcalls.js
@@ -0,0 +1,141 @@
+var DappCall = require("../utils/DappCall");
+var mailCall = require("../utils/mailCall");
+var logger = require("../utils/logger");
+var locker = require("../utils/locker");
+var blockWait = require("../utils/blockwait");
+var util = require("../utils/util");
+
+
+
+app.route.post("/issueTransactionCall", async function(req, res){
+    await locker("issueTransactionCall@"+req.query.pid);
+    logger.info("Entered /issueTransactionCall API");
+    var result = await issueAsset(req);
+    if(!result.isSuccess) return result;
+    await blockWait();
+    return result;
+})
+
+app.route.post("/issueTransactionCallMultiple", async function(req){
+    await locker("issueTransactionCallMultiple@" + req.query.iid);
+    logger.info("Entered /issueTransactionCallMultiple API");
+    var results = [];
+    for(i in req.query.pids){
+        var input = {
+            query: req.query
+        }
+        input.query.pid = req.query.pids[i];
+        results.push({
+            pid: req.query.pids[i],
+            result: await issueAsset(input)
+        });
+    }
+
+    await blockWait();
+
+    return {
+        results: results,
+        isSuccess: true
+    }
+});
+
+async function issueAsset(req){
+    try{
+    app.sdb.lock("finalIssueAsset@"+req.query.pid);
+    }catch(err){
+        return {
+            isSuccess: false,
+            message: "Same pid in a block"
+        }
+    }
+    var transactionParams = {};
+    var pid = req.query.pid;
+
+    var issuer = await app.model.Issuer.findOne({
+        condition: {
+            iid: req.query.iid
+        }
+    });
+
+    var issue = await app.model.Issue.findOne({
+        condition: {
+            pid: pid
+        }
+    });
+
+    if(!issue) return {
+        message: "Invalid Asset",
+        isSuccess: false
+    }
+
+    if(issue.status === 'issued') return {
+        message: "Asset already issued",
+        isSuccess: false
+    }
+
+    if(issue.status === 'pending') return {
+        message: "Asset not Authorized",
+        isSuccess: false
+    }
+
+    if(issue.iid !== req.query.iid) return {
+        message: "Invalid issuer",
+        isSuccess: false
+    }
+    
+    var employee = await app.model.Employee.findOne({
+        condition: {
+            empid: issue.empid
+        }
+    });
+    if(!employee) return {
+        message: "Invalid employee",
+        isSuccess: false
+    }
+    
+    // if(issue.status !== "authorized") return "Payslip not authorized yet";
+
+    var array = [employee.walletAddress, "payslip", JSON.parse(issue.data), issue.pid];
+
+    transactionParams.args = JSON.stringify(array);
+    transactionParams.type = 1003;
+    transactionParams.fee = req.query.fee;
+    transactionParams.secret = req.query.secret;
+    transactionParams.senderPublicKey = req.query.senderPublicKey;
+
+    console.log(JSON.stringify(transactionParams));
+
+    var response = await DappCall.call('PUT', "/unsigned", transactionParams, util.getDappID(),0);
+
+    if(!response.success) return {
+        isSuccess: false,
+        message: JSON.stringify(response)
+    }
+    
+    app.sdb.update('issue', {status: "issued"}, {pid: pid});  
+    app.sdb.update('issue', {timestampp: new Date().getTime()}, {pid: pid});  
+
+    var mailBody = {
+        mailType: "sendAssetIssued",
+        mailOptions: {
+            to: [employee.email],
+            payslip: JSON.parse(issue.data),
+            name: employee.name
+        }
+    }
+
+    mailCall.call("POST", "", mailBody, 0);
+
+    var activityMessage = issuer.email + " has issued payslip " + pid;
+    app.sdb.create('activity', {
+        activityMessage: activityMessage,
+        pid: pid,
+        timestampp: new Date().getTime(),
+        atype: 'payslip'
+    });
+
+    return {
+        isSuccess: true,
+        transactionId: response.transactionId
+    }
+}
